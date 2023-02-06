c6c607542f261fa8c3f31819b9a4ee86674b58f7
unknown <alirezaportous@gmail.com>
2023-02-06 21:26:00 +0330

    Fix dashboard blade staus color wasn't showing what we wanted to see, it wasn't loading dynamically correctly

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -23,8 +23,8 @@
                     <x-table.cell>{{$user->name}}</x-table.cell>
                     <x-table.cell>{{$user->email}}</x-table.cell>
                     <x-table.cell>
-                        <span class="inline-flex items-center gap-1 rounded-full bg-{{$user->status_color}}-50 px-2 py-1 text-xs font-semibold text-green-600">
-                            <span class="h-1.5 w-1.5 rounded-full bg-{{$user->status_color}}-600"></span>
+                        <span class="inline-flex items-center gap-1 rounded-full {{$user->status_color === 'red' ? 'bg-red-50 text-red-600' : 'bg-green-50 text-green-600'}} px-2 py-1 text-xs font-semibold">
+                            <span class="h-1.5 w-1.5 rounded-full {{$user->status_color === 'red' ? 'bg-red-600' : 'bg-green-600'}}"></span>
                             {{$user->status}}
                         </span>
                     </x-table.cell>
