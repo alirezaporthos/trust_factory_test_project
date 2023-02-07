9af8f3ef8a3af58419e758d6877113fdc93a8b96
unknown <alirezaportous@gmail.com>
2023-02-07 19:18:01 +0330

    Fix not filled token in invites error invite observer wasn't booted, added it to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Invite;
+use App\Observers\InviteObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Invite::observe(InviteObserver::class);
     }
 }
